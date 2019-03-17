4283c1a9cdd10247cb903fd29acb215cd4854c98
Anil Dhaubhadel <anildhaubhadel@unknown>
2019-03-17 21:08:20 +1100

    data persisted using UserDefault method

--- ToDo/ToDoViewController.swift
+++ ToDo/ToDoViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ToDoViewController: UITableViewController{
 
-    let itemArray = ["Apple", "ball", "cat"]
+    var itemArray = ["Apple", "ball", "cat"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
+            
+        
     }
 
     //MARK: - TableView Datasource Methods
@@ -44,6 +52,32 @@ class ToDoViewController: UITableViewController{
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.tableView.reloadData()
+            
+        }
+        alert.addAction(action)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
